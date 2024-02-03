edeaa803b0cda1abad943a0dbd9e8417c6f538af
Weiss Bai <whitesgr03@gmail.com>
2024-02-03 09:41:19 +0800

    Add catalogRouter

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
