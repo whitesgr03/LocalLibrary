6e86abcae2c3f0bfd6b78d87e38f73f6699d3b93
Weiss Bai <whitesgr03@gmail.com>
2024-02-04 01:29:19 +0800

    Format code

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -5,25 +5,25 @@ const BookInstance = require("../models/bookinstance");
 const bookinstance_list = asyncHandler(async (req, res,next) => {
 	res.send("NOT IMPLEMENTED: BookInstance list");
 });
-const bookinstance_detail = asyncHandler(async (req, res,next) => {
+const bookinstance_detail = asyncHandler(async (req, res, next) => {
 	res.send(`NOT IMPLEMENTED: BookInstance detail: ${req.params.id}`);
 });
-const bookinstance_create_get = asyncHandler(async (req, res,next) => {
+const bookinstance_create_get = asyncHandler(async (req, res, next) => {
 	res.send("NOT IMPLEMENTED: BookInstance create GET");
 });
-const bookinstance_create_post = asyncHandler(async (req, res,next) => {
+const bookinstance_create_post = asyncHandler(async (req, res, next) => {
 	res.send("NOT IMPLEMENTED: BookInstance create POST");
 });
-const bookinstance_delete_get = asyncHandler(async (req, res,next) => {
+const bookinstance_delete_get = asyncHandler(async (req, res, next) => {
 	res.send("NOT IMPLEMENTED: BookInstance delete GET");
 });
-const bookinstance_delete_post = asyncHandler(async (req, res,next) => {
+const bookinstance_delete_post = asyncHandler(async (req, res, next) => {
 	res.send("NOT IMPLEMENTED: BookInstance delete POST");
 });
-const bookinstance_update_get = asyncHandler(async (req, res,next) => {
+const bookinstance_update_get = asyncHandler(async (req, res, next) => {
 	res.send("NOT IMPLEMENTED: BookInstance update GET");
 });
-const bookinstance_update_post = asyncHandler(async (req, res,next) => {
+const bookinstance_update_post = asyncHandler(async (req, res, next) => {
 	res.send("NOT IMPLEMENTED: BookInstance update POST");
 });
 
