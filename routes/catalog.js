4c193101a533340136366cb0ec3034a964600bea
Weiss Bai <whitesgr03@gmail.com>
2024-02-03 09:32:27 +0800

    Format code

--- routes/catalog.js
+++ routes/catalog.js
@@ -6,6 +6,9 @@ const author_controller = require("../controllers/authorController");
 const genre_controller = require("../controllers/genreController");
 const book_instance_controller = require("../controllers/bookinstanceController");
 
+router.get("/", book_controller.index);
+router.get("/book/:id", book_controller.book_detail);
+router.get("/books", book_controller.book_list);
 router
 	.route("/book/create")
 	.get(book_controller.book_create_get)
@@ -21,10 +24,8 @@ router
 	.get(book_controller.book_update_get)
 	.post(book_controller.book_update_post);
 
-router.get("/", book_controller.index);
-router.get("/book/:id", book_controller.book_detail);
-router.get("/books", book_controller.book_list);
-
+router.get("/author/:id", author_controller.author_detail);
+router.get("/authors", author_controller.author_list);
 router
 	.route("/author/create")
 	.get(author_controller.author_create_get)
@@ -40,9 +41,8 @@ router
 	.get(author_controller.author_update_get)
 	.post(author_controller.author_update_post);
 
-router.get("/author/:id", author_controller.author_detail);
-router.get("/authors", author_controller.author_list);
-
+router.get("/genre/:id", genre_controller.genre_detail);
+router.get("/genres", genre_controller.genre_list);
 router
 	.route("/genre/create")
 	.get(genre_controller.genre_create_get)
@@ -58,9 +58,8 @@ router
 	.get(genre_controller.genre_update_get)
 	.post(genre_controller.genre_update_post);
 
-router.get("/genre/:id", genre_controller.genre_detail);
-router.get("/genres", genre_controller.genre_list);
-
+router.get("/bookinstance/:id", book_instance_controller.bookinstance_detail);
+router.get("/bookinstances", book_instance_controller.bookinstance_list);
 router
 	.route("/bookinstance/create")
 	.get(book_instance_controller.bookinstance_create_get)
@@ -75,8 +74,5 @@ router
 	.route("/bookinstance/:id/update")
 	.get(book_instance_controller.bookinstance_update_get)
 	.post(book_instance_controller.bookinstance_update_post);
-
-router.get("/bookinstance/:id", book_instance_controller.bookinstance_detail);
-router.get("/bookinstances", book_instance_controller.bookinstance_list);
 
 module.exports = router;
