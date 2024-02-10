5bc0b6361c7f8046bd65b3ca5abce81e15fbc52f
Weiss Bai <whitesgr03@gmail.com>
2024-02-10 12:11:48 +0800

    Change author_update_post functions
    We validation and sanitizes the data, then we use isEmpty to check whether there are any errors.
    
    If there are then we call  renderError function to render the form again with data by user entered and array of error messages.
    
    If the data is valid then we call isAuthorExists function to search to see if a new Author already exists.
    
    If the new Author already exists we redirect to its detail page.
    
    If not, we update the Author document and redirect to its detail page.

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -136,9 +136,67 @@ const author_update_get = asyncHandler(async (req, res, next) => {
 				author,
 		  });
 });
-const author_update_post = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: Author update POST");
-});
+const author_update_post = [
+	body("first_name")
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage("First name must be specified.")
+		.isAlphanumeric()
+		.withMessage("First name has non-alphanumeric characters."),
+	body("family_name")
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage("Family name must be specified.")
+		.isAlphanumeric()
+		.withMessage("Family name has non-alphanumeric characters."),
+	body("date_of_birth", "Invalid date of birth")
+		.optional({ values: "falsy" })
+		.isISO8601()
+		.toDate(),
+	body("date_of_death", "Invalid date of death")
+		.optional({ values: "falsy" })
+		.isISO8601()
+		.toDate(),
+	asyncHandler((req, res, next) => {
+		const errors = validationResult(req);
+		const author = new Author({
+			_id: req.params.id,
+			first_name: req.body.first_name,
+			family_name: req.body.family_name,
+			date_of_birth: req.body.date_of_birth,
+			date_of_death: req.body.date_of_death,
+		});
+
+		const isAuthorExists = async () => {
+			const authorExists = await Author.findOne({
+				first_name: req.body.first_name,
+				family_name: req.body.family_name,
+				date_of_birth: req.body.date_of_birth,
+				date_of_death: req.body.date_of_death,
+			}).exec();
+
+			const updateAuthor = async () => {
+				const updatedAuthor = await Author.findByIdAndUpdate(
+					req.params.id,
+					author
+				);
+				res.redirect(updatedAuthor.url);
+			};
+
+			authorExists ? res.redirect(authorExists.url) : updateAuthor();
+		};
+
+		!errors.isEmpty()
+			? res.render("author_form", {
+					title: "Update Author",
+					author,
+					errors: errors.array(),
+			  })
+			: isAuthorExists();
+	}),
+];
 
 module.exports = {
 	author_list,
